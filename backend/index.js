48f5d69fe8bdc8ff02e566cab752aa4598ed31b2
Sunny Jha <jhasunny550@gmail.com>
2026-01-02 00:34:10 +0530

    refactor: reorganize imports for improved readability and consistency

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,49 @@
-import express from 'express';
-const app = express();
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./config/db.js";
+import authRouter from "./Router/authRouter.js";
+import siteRouter from "./Router/siteRouter.js";
+
+import "./Jobs/monitor.job.js";
+
 dotenv.config();
-import {connectDB} from './config/db.js';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://web-health-checker.vercel.app'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, 
-  optionsSuccessStatus: 200 
+  origin: [
+    "http://localhost:3000",
+    "https://web-health-checker.vercel.app",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
 };
 
-
-
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware for token verification 
-
-import authRouter from './Router/authRouter.js';
 app.use("/api/auth", authRouter);
-
-import siteRouter from './Router/siteRouter.js';
 app.use("/api/site", siteRouter);
-
-import "./Jobs/monitor.job.js"; 
-
 
 const startServer = async () => {
   try {
-    await connectDB(); 
+    await connectDB();
     console.log("Database connected successfully");
 
     app.listen(PORT, () => {
-      console.log(`Server listening on http://localhost:${PORT}`);
+      console.log(`Server listening on port ${PORT}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
