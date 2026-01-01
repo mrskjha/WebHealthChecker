7843ea3e2e92e55119dee0eca7e88ef9d2e579c3
Sunny Jha <jhasunny550@gmail.com>
2026-01-02 01:16:42 +0530

    1

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: [
+    "https://webhealthchecker.onrender.com/",
     "http://localhost:3000",
     "https://web-health-checker.vercel.app",
   ],
